1c9374a43955b922f7883f6cdaf8bc36e5858c34
237rahul <31244306+237rahul@users.noreply.github.com>
2018-06-26 12:45:14 +0530

    Add files via upload

---
+++ WEEEK 1/DAY 1/PROBLEM 4.py
@@ -0,0 +1,79 @@
+import unittest
+
+
+def merge_ranges(meetings):
+
+    # Merge meeting ranges
+    meetings.sort()
+    merge=[]
+    for i in range(len(meetings)):
+        meetings[i]=list(meetings[i])
+    merge.append(meetings[0])
+    for i in meetings[1:]:
+        if(i[0]<=merge[len(merge)-1][1]):
+            merge[len(merge)-1][1]=max(i[1],merge[len(merge)-1][1])
+        else:
+            merge.append(i)
+    for i in range(len(merge)):
+        merge[i]=tuple(merge[i])
+        
+    return merge
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
